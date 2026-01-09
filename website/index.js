c2d7897d10eab9bb38e4ee281ccbd2e19123d697
JGGHalfhide <141737227+JGGHalfhide@users.noreply.github.com>
2026-01-09 14:34:42 -0500

    Update index.js

--- website/index.js
+++ website/index.js
@@ -2,7 +2,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://q47pjkw2xuqxfo6xaizd6dhp2q0uedyb.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ` This page has ${data} Views!`;
+    counter.innerHTML = ` This page has ${data} views`;
 }
 
 updateCounter();
